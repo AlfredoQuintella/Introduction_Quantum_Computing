7860139003b3beead2a4d96b090d5518190478ee
Alfredo Quintella <alfredo.quintella@gmail.com>
2024-03-10 23:54:10 -0300

    Create ex2.py

---
+++ Revisao_Complex/ex2.py
@@ -0,0 +1,17 @@
+def calcular_potencia_complexa(n):
+    resto_modulo_4 = n % 4
+    
+    if resto_modulo_4 == 0:
+        return 1
+    elif resto_modulo_4 == 1:
+        return complex(0, 1) 
+    elif resto_modulo_4 == 2:
+        return -1
+    elif resto_modulo_4 == 3:
+        return complex(0, -1) 
+
+n = int(input("Insira a potência desejada: "))
+
+resultado = calcular_potencia_complexa(n)
+
+print("Resultado:", resultado)
